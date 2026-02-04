b1c11e6bfcd8723c7d69b4599c9cdd45967b095c
Titus Ericson <Titusbianto38@gmail.com>
2026-02-04 23:13:39 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_112777_TitusEricsonBianto/week01/GameStore.kt
+++ src/oop_112777_TitusEricsonBianto/week01/GameStore.kt
@@ -3,13 +3,14 @@ package oop_112777_TitusEricsonBianto.week01
 fun main(){
     val gameTitle = "Titus Hero"
     val price = 900000
-
+    val userNote: String? = null
     val finalPrice = DiscCalculate(price)
 
     printReceipt(
         title = gameTitle,
         originalPrice = price,
-        finalPrice = finalPrice
+        finalPrice = finalPrice,
+        note = userNote
     )
 }
 
@@ -20,9 +21,12 @@ if (price > 500_000){
     (price * 0.8).toInt()
 }
 
-fun printReceipt(title: String, originalPrice: Int, finalPrice: Int) {
+fun printReceipt(title: String, originalPrice: Int, finalPrice: Int, note: String?) {
     println("=== SteamKW Receipt ===")
     println("Judul Game  : $title")
     println("Harga Asli  : Rp $originalPrice")
     println("Harga Akhir : Rp $finalPrice")
+
+    val notePrint = note ?: "Tidak ada Catatan"
+    println("Catatam     : $notePrint")
 }
