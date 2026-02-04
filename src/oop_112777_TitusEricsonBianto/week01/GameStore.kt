ae69b2ae5df5048f45781b09252465035e10b5ff
Titus Ericson <Titusbianto38@gmail.com>
2026-02-04 23:06:49 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_112777_TitusEricsonBianto/week01/GameStore.kt
+++ src/oop_112777_TitusEricsonBianto/week01/GameStore.kt
@@ -1,8 +1,17 @@
 package oop_112777_TitusEricsonBianto.week01
 
 fun main(){
-    val game = "Titus Hero"
+    val gameTitle = "Titus Hero"
     val price = 900000
 
-    println("Game: $game, Price: $price")
+    val finalPrice = DiscCalculate(price)
+
+    println("Game: $gameTitle, Harga Akhir: $finalPrice")
+}
+
+fun DiscCalculate(price: Int): Int =
+if (price > 500_000){
+    (price * 0.8).toInt()
+} else{
+    (price * 0.8).toInt()
 }
