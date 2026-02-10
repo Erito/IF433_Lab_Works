394fb48d803becb42bff2cc51ba0db99feae55ea
Titus Ericson <Titusbianto38@gmail.com>
2026-02-10 15:30:49 +0700

    week02: add warning validation in init block

--- src/oop_112777_TitusEricsonBianto/week02/Student.kt
+++ src/oop_112777_TitusEricsonBianto/week02/Student.kt
@@ -1,9 +1,12 @@
 package oop_112777_TitusEricsonBianto.week02
 
-class Student(
-    val name: String,
-    val nim: String,
-    var major: String
-){
-
+class Student(val name: String, val nim: String, var major: String){
+    init{
+        if(nim.length != 5){
+            println("WARNING: Objek tercipta dengan nim ($nim) yang tidak valid")
+            println("Data mahasiswa $name mungin akan bermasalah di sistem")
+        } else{
+            println("LOG: Objek Student $name berhasil dialokasikan di memory")
+        }
+    }
 }
