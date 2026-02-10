02545337682e6f99901616cbc8a533a6d37a849b
Titus Ericson <Titusbianto38@gmail.com>
2026-02-10 16:08:49 +0700

    week02: implement selection logic for constructors

--- src/oop_112777_TitusEricsonBianto/week02/Main.kt
+++ src/oop_112777_TitusEricsonBianto/week02/Main.kt
@@ -14,6 +14,22 @@ fun main(){
 
     scanner.nextLine() //Bersihkan buffer newline (penyakit klasik Scanner)
 
+    print("Pilih Jalur (1. Reguler, 2. Umum: )")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if(type == 1){
+        print("Masukan Jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa} ")
+    } else if(type == 2){
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa} ")
+    } else{
+        println("Pilihan ngawur")
+    }
+
     //Validasi di sisi pemanggil (Main)
     if(nim.length != 5){
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter")
--- src/oop_112777_TitusEricsonBianto/week02/Student.kt
+++ src/oop_112777_TitusEricsonBianto/week02/Student.kt
@@ -3,8 +3,8 @@ package oop_112777_TitusEricsonBianto.week02
 class Student(
     val name: String,
     val nim: String,
-    var gpa: Double = 0.0, // <-- Default Argument
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0 // <-- Default Argument
 ){
     init{
         if(nim.length != 5){
