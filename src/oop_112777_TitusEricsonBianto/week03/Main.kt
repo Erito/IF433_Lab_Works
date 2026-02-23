c39db22c0216c3eff304e3e878ccfa079ca6f230
Titus Ericson <Titusbianto38@gmail.com>
2026-02-23 22:00:56 +0700

    week03: add private property

--- src/oop_112777_TitusEricsonBianto/week03/Main.kt
+++ src/oop_112777_TitusEricsonBianto/week03/Main.kt
@@ -4,4 +4,9 @@ fun main() {
     val e = Employee("Titus")
     println("SIP")
     e.salary = -5
+    e.salary = 5000000 // 5juta
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+    println("Pajak yang harus dibayar ${e.tax}") // 500.000
 }
