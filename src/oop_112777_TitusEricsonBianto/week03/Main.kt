d2517150cb1ee94d6a6c729caf482b4951ac85f1
Titus Ericson <Titusbianto38@gmail.com>
2026-02-23 22:01:16 +0700

    week03: cleanup code

--- src/oop_112777_TitusEricsonBianto/week03/Main.kt
+++ src/oop_112777_TitusEricsonBianto/week03/Main.kt
@@ -4,9 +4,9 @@ fun main() {
     val e = Employee("Titus")
     println("SIP")
     e.salary = -5
-    e.salary = 5000000 // 5juta
+    e.salary = 5000000
     println("Gaji: ${e.salary}")
 
     e.increasePerformance()
-    println("Pajak yang harus dibayar ${e.tax}") // 500.000
+    println("Pajak yang harus dibayar ${e.tax}")
 }
