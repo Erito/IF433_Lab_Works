b5a657771f4369e5b080b7b7694385e530d6f96e
Titus Ericson <Titusbianto38@gmail.com>
2026-03-03 23:57:51 +0700

    week05: (task 2) create EWallet class

--- src/oop_112777_TitusEricsonBianto/week05/EWallet.kt
+++ src/oop_112777_TitusEricsonBianto/week05/EWallet.kt
@@ -1,3 +1,19 @@
 package oop_112777_TitusEricsonBianto.week05
 
 
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("[$accountName] Sukses! Pembayaran EWallet berhasil. Sisa saldo: $balance")
+        } else {
+            println("[$accountName] Saldo tidak cukup. Saldo saat ini: $balance")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("[$accountName] Top Up berhasil sebesar $amount. Saldo saat ini: $balance")
+    }
+}
