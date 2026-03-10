3cf4ff2069757ecce0eaba471e45289174463de3
Titus Ericson <Titusbianto38@gmail.com>
2026-03-10 15:19:09 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_112777_TitusEricsonBianto/Week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_112777_TitusEricsonBianto.Week06
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phonne{
+    fun turnOff(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
