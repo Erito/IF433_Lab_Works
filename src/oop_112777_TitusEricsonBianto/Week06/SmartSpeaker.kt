54160f77831f9d6e661491761d07ae7b4999cb42
Titus Ericson <Titusbianto38@gmail.com>
2026-03-10 22:19:40 +0700

    week06: (task) create SmartSpeaker

--- src/oop_112777_TitusEricsonBianto/Week06/SmartSpeaker.kt
+++ src/oop_112777_TitusEricsonBianto/Week06/SmartSpeaker.kt
@@ -4,7 +4,13 @@ class SmartSpeaker(
     override val id: String,
     override val name: String
 ) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("$id turn on")
+    }
+    override fun turnOff() {
+        println("$id turn off")
+    }
     fun playMusic(song: String) {
-        println("Memutar lagu $song dari Spotify")
+        println("Memutar lagu $song dari Spotify.")
     }
 }
