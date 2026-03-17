506c1671093d4a39248578740d97e87d0ef637bf
Titus Ericson <Titusbianto38@gmail.com>
2026-03-18 00:07:06 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_112777_TitusEricsonBianto/Week07/BattleEvent.kt
@@ -0,0 +1,12 @@
+package oop_112777_TitusEricsonBianto.Week07
+
+sealed class BattleState {
+
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+
+    data class LootDropped(val item: GameItem) : BattleState()
+
+    data class GameOver(val reason: String) : BattleState()
+
+    object SafeZone : BattleState()
+}
