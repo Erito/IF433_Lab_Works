99bc116c6d7760546eabccfddebb45cba3105eb4
Titus Ericson <Titusbianto38@gmail.com>
2026-03-17 23:55:19 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_112777_TitusEricsonBianto/Week07/Main.kt
+++ src/oop_112777_TitusEricsonBianto/Week07/Main.kt
@@ -8,4 +8,11 @@ fun main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient() // Instansiasi lewat Factory
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+
+    println(reg1) // Akan mencetak alamat memori (hash code), bukan data
+    println("Sama? ${reg1 == reg2}") // Akan menghasilkan False karena alamat memori berbeda
 }
---
+++ src/oop_112777_TitusEricsonBianto/Week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_112777_TitusEricsonBianto.Week07
+
+class RegularUser(val name: String, val age: Int)
