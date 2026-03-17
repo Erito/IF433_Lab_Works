3db7df893a48aac8f7ec46ea80946d4cb82ce350
Titus Ericson <Titusbianto38@gmail.com>
2026-03-18 00:01:58 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_112777_TitusEricsonBianto/Week07/Main.kt
+++ src/oop_112777_TitusEricsonBianto/Week07/Main.kt
@@ -37,6 +37,7 @@ fun main() {
     val uiMessage = when(response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        is ApiResponse.Loading -> "Sedang memuat..."
     }
 
     println(uiMessage)
