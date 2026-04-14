c1e843698d77aae485b8fc45fbfdac128883d11d
Titus Ericson <Titusbianto38@gmail.com>
2026-04-14 15:48:33 +0700

    week08: implement safe call with let block

--- src/oop_112777_TitusEricsonBianto/Week08/OrderModels.kt
+++ src/oop_112777_TitusEricsonBianto/Week08/OrderModels.kt
@@ -3,4 +3,4 @@ package oop_112777_TitusEricsonBianto.Week08
 class City(val name: String)
 class Address(val city: City?)
 class DeliveryDetails(val address: Address?)
-class order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
