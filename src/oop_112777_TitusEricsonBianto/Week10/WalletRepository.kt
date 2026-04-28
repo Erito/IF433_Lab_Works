e396e9544872eb3b2e9a5b91a866ef5ad3d08ef4
Titus Ericson <Titusbianto38@gmail.com>
2026-04-28 13:13:40 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_112777_TitusEricsonBianto/Week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_112777_TitusEricsonBianto.Week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
