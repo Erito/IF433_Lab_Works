11548cee10cc0147ece2c77d4d10a96edf098dce
Titus Ericson <Titusbianto38@gmail.com>
2026-04-28 13:16:06 +0700

    week10: (task) populate coin repository

--- src/oop_112777_TitusEricsonBianto/Week10/CryptoDashboard.kt
+++ src/oop_112777_TitusEricsonBianto/Week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_112777_TitusEricsonBianto.Week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 4.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
