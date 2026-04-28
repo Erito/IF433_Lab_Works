7bb062f3b3d5ccd846a4e0c8783204683f59b43d
Titus Ericson <Titusbianto38@gmail.com>
2026-04-28 13:10:55 +0700

    week10: implement type constraint for numeric types

---
+++ src/oop_112777_TitusEricsonBianto/Week10/NumericalStorage.kt
@@ -0,0 +1,7 @@
+package oop_112777_TitusEricsonBianto.Week10
+
+class MathBox<T: Number>(val value1: T, val value2: T) {
+    fun sum(): Double {
+        return value1.toDouble() + value2.toDouble()
+    }
+}
