82c420a4c122d1d22cb78d8a9952d59498202fa7
Titus Ericson <Titusbianto38@gmail.com>
2026-05-05 13:20:44 +0700

    week11: implement let function for null safety

--- src/tes.kt
+++ src/tes.kt
@@ -2,6 +2,24 @@ fun String.DasarExtenson():String{
     return "Halo $this"
 }
 
+fun String.RubahHurudBesarDepan():String{
+    var hasil = ""
+    hasil = this.split(" ").joinToString(" "){
+        it.replaceFirstChar {
+            c -> c.uppercase()
+        }
+    }
+    return hasil
+}
+
 fun main(){
     println("HI".DasarExtenson())
+
+
+    //Tanpa extension func
+    val nama1 = "Budi Sihombing"
+    println(nama1.uppercase())
+
+    //dengan extension function
+    println("budi kurniawan".RubahHurudBesarDepan())
 }
