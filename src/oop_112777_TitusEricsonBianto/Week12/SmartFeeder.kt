392d130f08d889f3944b69ea7336a6da4d5c90b6
Titus Ericson <Titusbianto38@gmail.com>
2026-05-12 12:55:25 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_112777_TitusEricsonBianto/Week12/SmartFeeder.kt
@@ -0,0 +1,8 @@
+package oop_112777_TitusEricsonBianto.Week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    // Blok validasi akan ditambahkan di langkah selanjutnya
+    return availableGram
+}
