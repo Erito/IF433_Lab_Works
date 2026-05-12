f5a2b3fdbb4af88f6cb1d025a2a198b3dea75ca9
Titus Ericson <Titusbianto38@gmail.com>
2026-05-12 13:03:33 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_112777_TitusEricsonBianto/Week12/SmartFeeder.kt
+++ src/oop_112777_TitusEricsonBianto/Week12/SmartFeeder.kt
@@ -2,7 +2,7 @@ package oop_112777_TitusEricsonBianto.Week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-
+//tes
     if (isJammed) {
         throw DispenserJamException()
     }
