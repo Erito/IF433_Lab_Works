c88f73f978b9fb60d7ffeb4d6842da74e612fdf7
Titus Ericson <Titusbianto38@gmail.com>
2026-05-12 12:56:00 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_112777_TitusEricsonBianto/Week12/SmartFeeder.kt
+++ src/oop_112777_TitusEricsonBianto/Week12/SmartFeeder.kt
@@ -3,6 +3,10 @@ package oop_112777_TitusEricsonBianto.Week12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
-    // Blok validasi akan ditambahkan di langkah selanjutnya
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
     return availableGram
 }
+
