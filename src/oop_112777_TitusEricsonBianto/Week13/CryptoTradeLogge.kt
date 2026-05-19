31f31bc135df1727945b74297deda37cd3ee97ff
Titus Ericson <Titusbianto38@gmail.com>
2026-05-19 18:18:51 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_112777_TitusEricsonBianto/Week13/CryptoTradeLogge.kt
@@ -0,0 +1,6 @@
+package oop_112777_TitusEricsonBianto.Week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
