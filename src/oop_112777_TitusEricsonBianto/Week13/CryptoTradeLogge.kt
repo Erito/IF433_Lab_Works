0e31be94b129455070e3bf10063987a53b3ecd03
Titus Ericson <Titusbianto38@gmail.com>
2026-05-19 18:23:13 +0700

    week13: (task) inject malformed line via appendText to test robustness

--- src/oop_112777_TitusEricsonBianto/Week13/CryptoTradeLogge.kt
+++ src/oop_112777_TitusEricsonBianto/Week13/CryptoTradeLogge.kt
@@ -47,4 +47,5 @@ fun main() {
         TradeRecord(2, "ETHUSDT", "Short", 500.0, -20.0)
     )
     saveTrades(myTrades, "crypto_trades.csv")
+    File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
 }
