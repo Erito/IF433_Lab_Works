42f9f5b8cd341ca59683103b8e8033939bfe7e0a
Titus Ericson <Titusbianto38@gmail.com>
2026-05-26 14:05:20 +0700

    week14: setup project structure

--- src/tes.kt
+++ src/tes.kt
@@ -1,98 +1,51 @@
 
-fun pembagian(){
-    try{
-        val a = 10
-        val b = 5
-        val hasil = a/b
-        println("Hasil bagi: $hasil")
-    }catch(e: Exception){
-        println("Ada Error:  " + e.message)
-    }finally {
-        println("Selesai try catch")
+class proses_khs {
+    fun hitung_nilai_tugas(quiz: Double, aktivitas: Double): Double {
+        return (quiz * 0.3) + (aktivitas * 0.7)
+    }
+
+    fun hitung_nilai_akhir(utsMhs: Double, uasMhs: Double, tugasMhs: Double, jnsKur: jenis_kurikulum): Double {
+        return jnsKur.hitung_nilai_bobot(utsMhs, uasMhs, tugasMhs)
     }
 }
 
-fun cek_tipe_variable(){
-    var angka:Int = try {
-        Integer.parseInt("123")
-    }catch (e: Exception){
-        println("ada Error:  " + e.message)
-        6767
+class db_khs {
+    fun simpan_na_db(nim: String, nama: String, nilaiAkhir: Double): String {
+        return "KHS $nim nama $nama dapat nilai $nilaiAkhir"
     }
-    println(angka)
-}
 
-fun cek_nilai(nilai:Int){
-    if(nilai<0){
-        throw IllegalArgumentException("Masa Nilai minus")
-    } else if(nilai > 100){
-        println("Masa Nilai lebih besar dari 100")
-    } else{
-        println("Nilai kamu adalah " + nilai)
+    fun load_db(nim: String): String {
+        return "Berhasil load data $nim"
     }
 }
 
-class cek_saldo_rekening(val pengeluaran: Int, val saldo:Int ):
-    Exception("Saldo kamu sisa $saldo dan pengeluaran: $pengeluaran")
-class transaksi_keuangan(){
-    fun narik_uang(balance:Int, totalBelanja:Int){
-        if(totalBelanja> balance){
-            try{
-                throw cek_saldo_rekening(totalBelanja, balance)
-            } catch (e:Exception){
-                println(e.message)
-            }
-        } else{
-            println("Transaksi berhasil, sisa saldo ${balance - totalBelanja}")
-        }
+interface jenis_kurikulum {
+    fun hitung_nilai_bobot(uts: Double, uas: Double, tugas: Double): Double
+}
+
+class kurikulum_2013 : jenis_kurikulum {
+    override fun hitung_nilai_bobot(uts: Double, uas: Double, tugas: Double): Double {
+        return (uts * 0.3) + (uas * 0.4) + (tugas * 0.3)
     }
 }
 
-fun multiple_catch(input:String){
-    try{
-        val angka:Int = input.toInt()
-        val hitungBagi: Int = angka/0
-        println("Hasil bagi $hitungBagi")
-    } catch(e: NumberFormatException){
-        println("GBISA BAGI HURUF " + e.message)
-    } catch(e: ArithmeticException){
-        println("Masa pembagian pake 0: ${e.message}")
-    } catch (e:Exception){
-        println("Ada errpr di multiple catch: ${e.message}")
+class kurikulum_merdeka : jenis_kurikulum {
+    override fun hitung_nilai_bobot(uts: Double, uas: Double, tugas: Double): Double {
+        return (uts * 0.2) + (uas * 0.5) + (tugas * 0.3)
     }
 }
 
-sealed class BankException(pesan:String): Exception("Error di Bank EXCEPTION " + pesan)
-class cek_saldo(val pengeluaran:Int): BankException("Pengeluaran kamu $pengeluaran dari saldo")
-class cek_input(val transaksi:Int): BankException("Transaksi kamu masa minus $transaksi")
+fun main() {
+    val khsMhs = proses_khs()
+    val nilaiTugas = khsMhs.hitung_nilai_tugas(83.57, 76.45)
+    val naMhs1: Double = khsMhs.hitung_nilai_akhir(85.67, 70.40, nilaiTugas, kurikulum_2013())
+    val naMhs2: Double = khsMhs.hitung_nilai_akhir(85.67, 70.40, nilaiTugas, kurikulum_merdeka())
 
-fun transaksi_belanja(saldoKamu: Int, jajanKamu: Int):Int{
-    if(jajanKamu < 0){
-        throw cek_input(jajanKamu)
-    } else if(saldoKamu < jajanKamu){
-        throw cek_saldo(jajanKamu)
-    }
-    return saldoKamu - jajanKamu
-}
+    val dbMhs = db_khs()
+    val statSimpan1: String = dbMhs.simpan_na_db("123", "Titus", naMhs1)
+    val statSimpan2: String = dbMhs.simpan_na_db("123", "Titus", naMhs2)
 
-fun main(){
-    println("Testing dulu")
-    pembagian()
-    cek_tipe_variable()
 
-    try {
-        cek_nilai(101)
-    } catch(e:Exception){
-        println("Wow ada error di niai: " + e.message)
-    }
-
-    val trx = transaksi_keuangan()
-    trx.narik_uang(1200, 1000)
-
-    multiple_catch("as^d")
-    println("")
-
-    runCatching {
-        transaksi_belanja(1000, 1200)
-    }.onSuccess {println("Belanja Berhasil , sisa saldo: $it")}.onFailure {println(it)}
+    println(statSimpan1)
+    println(statSimpan2)
 }
