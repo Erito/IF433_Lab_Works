71d8ba3ce69841f413af5cbdfe1a24da63e5ebe1
Titus Ericson <Titusbianto38@gmail.com>
2026-06-08 11:55:25 +0700

    yes

--- src/latihanUAS/Latihan1.kt
+++ src/latihanUAS/Latihan1.kt
@@ -1,70 +1,65 @@
 package latihanUAS
 
-interface ITrackable {
+interface ITrackable{
     fun updateStatus(newStatus: String)
 }
 
-abstract class DeliverableItem(val id: String, val weight: Double):ITrackable {
+abstract class DeliverableItem(val id: String, val weight: Double): ITrackable{
     var status: String = "Pending"
     var courierName: String? = null
-    override fun updateStatus(newStatus: String) {
+    override fun updateStatus(newStatus: String){
         status = newStatus
     }
 }
 
-class Document(id: String, weight: Double, val isConfidential: Boolean) : DeliverableItem(id, weight)
-class Parcel (id:String, weight: Double, val isFragile: Boolean):DeliverableItem(id, weight)
+
+class Document(id: String, weight: Double, val isConfidential: Boolean ): DeliverableItem(id, weight)
+class Parcel(id: String, weight: Double, val isFragile: Boolean ): DeliverableItem(id, weight)
 
 class DeliveryRepository<T>{
     private val items = mutableListOf<T>()
     fun addItem(item: T){
         items.add(item)
     }
-    fun getAllItems(): List<T> {
+    fun getAllItems(): List<T>{
         return items
     }
 }
 
 fun DeliverableItem.printDetails(){
-    val courier = this.courierName?: "Belum dialokasikan"
-    println("[ID] $id Berat: $weight kg, Status: $status, Kurir: $courier")
+    val courier = this.courierName?: "Belum Dialokasikan"
+    println("[$id] Berat: [$weight]kg | Status: [$status] | Kurir: [$courierName]")
 }
 
-fun main(){
+fun main() {
     val repo = DeliveryRepository<DeliverableItem>()
-    val doc1 = Document("A1", 50.00, true).apply {
-        courierName = "Titus"
+    val doc1 = Document("DOC-001", 0.5, true).apply {
+        courierName = "Budi"
     }
-    val parcel1 = Parcel("A2", 50.0, true).apply{
-        courierName = "Claresta"
+    val parcel1 = Parcel("PAR-001", 2.5, true).apply {
+        courierName = "Andi"
     }
-    val parcel2= Parcel("A3", 50.0, false). apply {
+    val parcel2 = Parcel("PAR-002", 10.0, false).apply {
 
     }
 
+    repo.addItem(doc1)
     repo.addItem(parcel1)
     repo.addItem(parcel2)
-    repo.addItem(doc1)
-
     doc1.updateStatus("Delivered")
-    parcel1.updateStatus("Delivered")
 
-    println("Semua Pengiriman")
-    repo.getAllItems().forEach{
-        item -> item.printDetails()
+    println("--SEMUA PENGIRIMAN--")
+    repo.getAllItems().forEach { item ->
+        item.printDetails()
     }
 
-    println("Paket yang Pending")
-    val pendingPaket = repo.getAllItems().filter{
-        it.status == "Pending"
-    }
-
-    pendingPaket.let {
-        yas ->
-        if(yas.isEmpty()){
-            println("Tidak ada Paket Penting")
-        } else{
-            yas.forEach { it.printDetails()}
+    println("\n--DAFTAR PAKET PENDING--")
+    val pendingItems = repo.getAllItems().filter { it.status == "Pending" }
+    pendingItems.let { list ->
+        if (list.isEmpty()) {
+            println("Tidak ada paket pending.")
+        } else {
+            list.forEach { it.printDetails() }
         }
     }
 }
